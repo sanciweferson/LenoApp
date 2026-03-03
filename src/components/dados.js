7712c2019b2714e183ddf1a2521d9c6bc58d5b2c
Sanciweferson <sanciwefergy@gmail.com>
2026-03-03 11:34:35 -0300

    fix: corrige caminho da logo para produção

--- src/components/dados.js
+++ src/components/dados.js
@@ -1,5 +1,7 @@
+
+
 export const Icons = {
-  logo: `<img src="../../public/logo.svg" alt="Logo leno app" />`,
+  logo: `<img src="/logo.svg" alt="Logo leno app" />`,
   hamburger: `<svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 6h16M4 12h16M4 18h16"/></svg>`,
   close: `<svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 6L6 18M6 6l12 12"/></svg>`,
   home: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>`,
